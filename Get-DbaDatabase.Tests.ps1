a04f7c8efa3cd881014d55418e07cf0bd3023d5d
ctrlbold <clemaire@gmail.com>
2017-05-18 20:41:15 +0200

    added info

--- Get-DbaDatabase.Tests.ps1
+++ Get-DbaDatabase.Tests.ps1
@@ -1,5 +1,8 @@
 #Thank you Warren http://ramblingcookiemonster.github.io/Testing-DSC-with-Pester-and-AppVeyor/
 
+# For the developer, leave the above thanks in. Remove this and an important thing to note
+# we have two instances that appveyor tests against localhost (sql2008r2) and localhost\sql2016
+# the appveyor-lab is used to build sample objects that can be tested, talk to Chrissy
 Describe "Get-DbaDatabase Integration Tests" -Tags "Integrationtests" {
 
     Context "Count system databases on localhost" {
