60bd82f36a43b4468ac74ec5d91570cbfc744189
Shawn Melton <wshawnmelton@gmail.com>
2017-07-26 20:42:37 -0500

    Ok adding back originals for safe keeping

---
+++ Archive/Get-DbaSmoObject.ps1
@@ -0,0 +1,88 @@
+function Get-DbaJobCategory {
+	<#
+		.SYNOPSIS
+			Gets SQL Agent Job Category information for each instance(s) of SQL Server.
+
+		.DESCRIPTION
+			The Get-DbaJobCategory returns connected SMO object for SQL Agent Job Category information for each instance(s) of SQL Server.
+			
+		.PARAMETER SqlInstance
+			SQL Server name or SMO object representing the SQL Server to connect to. This can be a collection and receive pipeline input to allow the function to be executed against multiple SQL Server instances.
+
+		.PARAMETER SqlCredential
+			SqlCredential object to connect as. If not specified, current Windows login will be used.
+
+		.PARAMETER JobCategory
+			The job category(ies) to process. This list is auto populated from the server. If unspecified, all job categories will be processed.
+
+		.PARAMETER ExcludeJobCategory
+			The job category(ies) to exclude. This list is auto populated from the server.
+
+		.PARAMETER Silent
+			If this switch is enabled, the internal messaging functions will be silenced. 
+		
+		.NOTES
+			Original Author: FirstName LastName (@twitterhandle and/or website)
+			Tags: Migration, Backup
+			
+			Website: https://dbatools.io
+			Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+			License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+
+		.LINK
+			https://dbatools.io/Get-DbaJobCategory
+
+		.EXAMPLE
+			Get-DbaJobCategory -SqlInstance localhost
+
+			Returns all SQL Agent Job Categories on the local default SQL Server instance
+
+		.EXAMPLE
+			Get-DbaJobCategory -SqlInstance localhost, sql2016
+
+			Returns all SQL Agent Job Categories for the local and sql2016 SQL Server instances
+	#>
+	[CmdletBinding()]
+	param (
+		[parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $True)]
+		[Alias("ServerInstance", "SqlServer")]
+		[DbaInstanceParameter[]]$SqlInstance,
+		[Alias("Credential")]
+		[PSCredential][System.Management.Automation.CredentialAttribute()]
+		$SqlCredential,
+		[object[]]$JobCategory,
+		[object[]]$ExcludeJobCategory,
+		[switch]$Silent
+	)
+	process {
+		foreach ($instance in $SqlInstance) {
+			Write-Message -Level Verbose -Message "Attempting to connect to $instance"
+			
+			try {
+				$server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $SqlCredential
+			}
+			catch {
+				Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
+			}
+			
+			$categories = $server.JobServer.JobCategory
+			
+			if ($JobCategory) {
+				$categories = $categories | Where-Object Name -in $JobCategory
+			}
+			if ($ExcludeJobCategory) {
+				$categories = $categories | Where-Object Name -notin $ExcludeJobCategory
+			}
+			
+			foreach ($object in $categories) {
+				Write-Message -Level Verbose -Message "Processing $object"
+				Add-Member -Force -InputObject $object -MemberType NoteProperty ComputerName -value $server.NetName
+				Add-Member -Force -InputObject $object -MemberType NoteProperty InstanceName -value $server.ServiceName
+				Add-Member -Force -InputObject $object -MemberType NoteProperty SqlInstance -value $server.DomainInstanceName
+				
+				# Select all of the columns you'd like to show
+				Select-DefaultView -InputObject $object -Property ComputerName, InstanceName, SqlInstance, ID, Name, Whatever, Whatever2
+			}
+		}
+	}
+}
---
+++ Archive/Remove-Set-Write-DbaWhatever.ps1
@@ -0,0 +1,174 @@
+function Remove-DbaDatabase {
+	<#
+		.SYNOPSIS
+			Drops a database, hopefully even the really stuck ones.
+
+		.DESCRIPTION
+			Tries a bunch of different ways to remove a database or two or more.
+
+		.PARAMETER SqlInstance
+			The SQL Server instance holding the databases to be removed.You must have sysadmin access and server version must be SQL Server version 2000 or higher.
+
+		.PARAMETER SqlCredential
+			Allows you to login to servers using SQL Logins instead of Windows Authentication (AKA Integrated or Trusted). To use:
+
+			$cred = Get-Credential, this pass this $cred to the param. 
+		
+			Windows Authentication will be used if SqlCredential is not specified. SQL Server does not accept Windows credentials being passed as credentials. To connect as a different Windows user, run PowerShell as that user.
+
+		.PARAMETER Database
+			The database(s) to process. This list is auto-populated from the server. If unspecified, all databases will be processed.
+
+		.PARAMETER ExcludeDatabase
+			The database(s) to exclude - this list is auto-populated from the server.
+
+		.PARAMETER WhatIf
+			If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
+
+		.PARAMETER Confirm
+			If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
+
+		.PARAMETER Silent
+			If this switch is enabled, the internal messaging functions will be silenced.
+
+		.NOTES
+			Tags: Migration, Backup
+			Original Author: FirstName LastName (@twitterhandle and/or website)
+
+			Website: https://dbatools.io
+			Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+			License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+
+		.LINK
+			https://dbatools.io/Remove-DbaDatabase
+
+		.EXAMPLE
+			Remove-DbaDatabase -SqlInstance sql2016 -Database containeddb
+
+			Prompts then removes the database containeddb on SQL Server sql2016
+
+		.EXAMPLE
+			Remove-DbaDatabase -SqlInstance sql2016 -ExcludeDatabase mydb, containeddb
+
+			Prompts then removes all the user databases except mydb and containeddb on SQL Server sql2016.
+
+		.EXAMPLE
+			Remove-DbaDatabase -SqlInstance sql2016 -Database containeddb, mydb
+
+			Prompts then removes the databases containeddb and mydb on SQL Server sql2016
+
+		.EXAMPLE
+			Remove-DbaDatabase -SqlInstance sql2016 -Database containeddb -Confirm:$false
+
+			Does not prompt and swiftly removes containeddb on SQL Server sql2016
+	#>
+	[CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Low')]
+	param (
+		[parameter(Mandatory = $true, ValueFromPipeline = $true)]
+		[Alias("ServerInstance", "SqlServer")]
+		[DbaInstanceParameter[]]$SqlInstance,
+		[parameter(Mandatory = $false)]
+		[Alias("Credential")]
+		[PSCredential][System.Management.Automation.CredentialAttribute()]
+		$SqlCredential,
+		[parameter(Mandatory)]
+		[object[]]$Database,
+		[object[]]$ExcludeDatabase,
+		[switch]$Silent
+	)
+
+	begin {
+		<# include any input validations #>
+		<# if you need the validation to stop the function further utilize following example #>
+		$exists = Test-Path $InputVariable
+		if (!$exists) {
+			Stop-Function -Message "Input variable passed in is invalid"
+			return
+		}
+	}
+	process {
+		if (Test-FunctionInterrupt) { return }
+
+		foreach ($instance in $SqlInstance) {
+			try {
+				Write-Message -Level Verbose -Message "Connecting to $instance"
+				$server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $SqlCredential
+			}
+			catch {
+				Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
+			}
+
+			if ($server.MajorVersion -lt 9) {
+				Stop-Function -Message "Performing x process is not supported in this version of SQL Server"
+				return
+			}
+			$databases = $server.Databases | Where-Object Name -in $Database
+			if ($ExcludeDatabase) {
+				$databases = $server.Databases | Where-Object Name -notin $ExcludeDatabase
+			}
+
+			foreach ($db in $databases) {
+				try {
+					if ($Pscmdlet.ShouldProcess("$db on $server", "KillDatabase")) {
+						$server.KillDatabase($db.name)
+						$server.Refresh()
+
+						[pscustomobject]@{
+							ComputerName = $server.NetName
+							InstanceName = $server.ServiceName
+							SqlInstance = $server.DomainInstanceName
+							Database = $db.name
+							Status = "Dropped"
+						}
+					}
+				}
+				catch {
+					try {
+						if ($Pscmdlet.ShouldProcess("$db on $server", "alter db set single_user with rollback immediate then drop")) {
+							$null = $server.ConnectionContext.ExecuteNonQuery("alter database $db set single_user with rollback immediate; drop database $db")
+
+							[pscustomobject]@{
+								ComputerName = $server.NetName
+								InstanceName = $server.ServiceName
+								SqlInstance = $server.DomainInstanceName
+								Database = $db.name
+								Status = "Dropped"
+							}
+						}
+					}
+					catch {
+						try {
+							if ($Pscmdlet.ShouldProcess("$db on $server", "SMO drop")) {
+								$server.Databases[$dbname].Drop()
+								$server.Refresh()
+
+								[pscustomobject]@{
+									ComputerName = $server.NetName
+									InstanceName = $server.ServiceName
+									SqlInstance = $server.DomainInstanceName
+									Database = $db.Name
+									Status = "Dropped"
+								}
+							}
+						}
+						catch {
+							Write-Message -Level Verbose -Message "Could not drop database $db on $server"
+
+							[pscustomobject]@{
+								ComputerName = $server.NetName
+								InstanceName = $server.ServiceName
+								SqlInstance = $server.DomainInstanceName
+								Database = $db.Name
+								Status = $_
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	end {
+		if (Test-FunctionInterrupt) { return }
+		<# any cleanup needed #>
+	}
+}
