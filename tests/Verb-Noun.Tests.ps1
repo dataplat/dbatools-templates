0c38bfa04c09edef9c6f62bb9b0c4459e6a05d5c
Shawn Melton <wshawnmelton@gmail.com>
2017-09-06 15:06:15 -0500

    updated test template
    Removed reference to specific command in parameter test block

--- tests/Verb-Noun.Tests.ps1
+++ tests/Verb-Noun.Tests.ps1
@@ -33,7 +33,7 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
 		#>
 		$paramCount = x
 		$defaultParamCount = x
-		[object[]]$params = (Get-ChildItem function:\Get-DbaAgReplica).Parameters.Keys
+		[object[]]$params = (Get-ChildItem function:\Verb-DbaXyz).Parameters.Keys
 		$knownParameters = 'Computer', 'SqlInstance', 'SqlCredential', 'Credential', 'Silent'
 		it "Should contain our specific parameters" {
 			( (Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count ) | Should Be $paramCount
