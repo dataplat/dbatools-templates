d3b4667b15ed574bce59a4d4ecdd5e5105bbe7db
Shawn Melton <wshawnmelton@gmail.com>
2018-06-19 07:48:37 -0500

    update license

--- Remove-Set-Write-DbaWhatever.ps1
+++ Remove-Set-Write-DbaWhatever.ps1
@@ -35,7 +35,7 @@
 
             Website: https://dbatools.io
             Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
-            License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+-           License: MIT https://opensource.org/licenses/MIT
 
         .LINK
             https://dbatools.io/Remove-DbaDatabase
@@ -70,7 +70,6 @@
         [parameter(Mandatory)]
         [object[]]$Database,
         [object[]]$ExcludeDatabase,
-        [Alias('Silent')]
         [switch]$EnableException
     )
 
