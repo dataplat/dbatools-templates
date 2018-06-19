31cb943dd1a43b08725e590ab16f9ebcfb214de7
Shawn Melton <wshawnmelton@gmail.com>
2018-06-19 07:46:47 -0500

    update license

--- Get-DbaSmoObject.ps1
+++ Get-DbaSmoObject.ps1
@@ -24,12 +24,12 @@ function Get-DbaJobCategory {
             Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 
         .NOTES
-            Author: FirstName LastName (@twitterhandle and/or website)
             Tags: Migration, Backup
+            Author: FirstName LastName (@twitterhandle and/or website)
 
             Website: https://dbatools.io
             Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
-            License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+-           License: MIT https://opensource.org/licenses/MIT
 
         .LINK
             https://dbatools.io/Get-DbaJobCategory
