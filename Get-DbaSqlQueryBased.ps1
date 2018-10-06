466a80bb808a416ecce70d83ff651fbe0baef162
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2018-10-06 10:27:41 +0200

    updated command

--- Get-DbaSqlQueryBased.ps1
+++ Get-DbaSqlQueryBased.ps1
@@ -34,7 +34,7 @@ function Get-DbaIdentityUsage {
             Tags: Identity
 
             Website: https://dbatools.io
-            Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+            Copyright (c) 2018 by dbatools, licensed under MIT
 -           License: MIT https://opensource.org/licenses/MIT
 
         .LINK
