0a59f15be220d809d6a411327088257a4614cce0
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2018-10-06 10:28:32 +0200

    updated copyright

--- Remove-Set-Write-DbaWhatever.ps1
+++ Remove-Set-Write-DbaWhatever.ps1
@@ -34,7 +34,7 @@
             Author: FirstName LastName (@twitterhandle and/or website)
 
             Website: https://dbatools.io
-            Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+            Copyright (c) 2018 by dbatools, licensed under MIT
 -           License: MIT https://opensource.org/licenses/MIT
 
         .LINK
