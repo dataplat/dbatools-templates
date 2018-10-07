f1d36aab42da04f1cf0bac3c4609d2a2629c925b
Shawn Melton <wshawnmelton@gmail.com>
2018-10-06 20:29:38 -0500

    update cbh

--- Remove-Set-Write-DbaWhatever.ps1
+++ Remove-Set-Write-DbaWhatever.ps1
@@ -1,66 +1,66 @@
 ﻿function Remove-DbaDatabase {
-<#
-    .SYNOPSIS
-        Drops a database, hopefully even the really stuck ones.
-        
-    .DESCRIPTION
-        Tries a bunch of different ways to remove a database or two or more.
-        
-    .PARAMETER SqlInstance
-        The SQL Server instance holding the databases to be removed.You must have sysadmin access and server version must be SQL Server version 2000 or higher.
-        
-    .PARAMETER SqlCredential
-        Login to the target instance using alternate Windows or SQL Login Authentication. Accepts credential objects (Get-Credential).
-        
-    .PARAMETER Database
-        The database(s) to process. This list is auto-populated from the server. If unspecified, all databases will be processed.
-        
-    .PARAMETER ExcludeDatabase
-        The database(s) to exclude - this list is auto-populated from the server.
-        
-    .PARAMETER WhatIf
-        If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
-        
-    .PARAMETER Confirm
-        If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
-        
-    .PARAMETER EnableException
-        By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
-        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
-        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
-        
-    .NOTES
-        Tags: Migration, Backup
-        Author: FirstName LastName (@twitterhandle and/or website)
-        
-        Website: https://dbatools.io
-        Copyright: (c) 2018 by dbatools, licensed under MIT
-        -           License: MIT https://opensource.org/licenses/MIT
-        
-    .LINK
-        https://dbatools.io/Remove-DbaDatabase
-        
-    .EXAMPLE
-        Remove-DbaDatabase -SqlInstance sql2016 -Database containeddb
-        
-        Prompts then removes the database containeddb on SQL Server sql2016
-        
-    .EXAMPLE
-        Remove-DbaDatabase -SqlInstance sql2016 -ExcludeDatabase mydb, containeddb
-        
-        Prompts then removes all the user databases except mydb and containeddb on SQL Server sql2016.
-        
-    .EXAMPLE
-        Remove-DbaDatabase -SqlInstance sql2016 -Database containeddb, mydb
-        
-        Prompts then removes the databases containeddb and mydb on SQL Server sql2016
-        
-    .EXAMPLE
-        Remove-DbaDatabase -SqlInstance sql2016 -Database containeddb -Confirm:$false
-        
-        Does not prompt and swiftly removes containeddb on SQL Server sql2016
-        
-#>
+    <#
+        .SYNOPSIS
+            Drops a database, hopefully even the really stuck ones.
+
+        .DESCRIPTION
+            Tries a bunch of different ways to remove a database or two or more.
+
+        .PARAMETER SqlInstance
+            The SQL Server instance holding the databases to be removed.You must have sysadmin access and server version must be SQL Server version 2000 or higher.
+
+        .PARAMETER SqlCredential
+            Login to the target instance using alternate Windows or SQL Login Authentication. Accepts credential objects (Get-Credential).
+
+        .PARAMETER Database
+            The database(s) to process. This list is auto-populated from the server. If unspecified, all databases will be processed.
+
+        .PARAMETER ExcludeDatabase
+            The database(s) to exclude - this list is auto-populated from the server.
+
+        .PARAMETER WhatIf
+            If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
+
+        .PARAMETER Confirm
+            If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
+
+        .PARAMETER EnableException
+            By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+            This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+            Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+
+        .NOTES
+            Tags: Migration, Backup
+            Author: FirstName LastName (@twitterhandle and/or website)
+
+            Website: https://dbatools.io
+            Copyright: (c) 2018 by dbatools, licensed under MIT
+            -           License: MIT https://opensource.org/licenses/MIT
+
+        .LINK
+            https://dbatools.io/Remove-DbaDatabase
+
+        .EXAMPLE
+            PS C:\> Remove-DbaDatabase -SqlInstance sql2016 -Database containeddb
+
+            Prompts then removes the database containeddb on SQL Server sql2016
+
+        .EXAMPLE
+            PS C:\> Remove-DbaDatabase -SqlInstance sql2016 -ExcludeDatabase mydb, containeddb
+
+            Prompts then removes all the user databases except mydb and containeddb on SQL Server sql2016.
+
+        .EXAMPLE
+            PS C:\> Remove-DbaDatabase -SqlInstance sql2016 -Database containeddb, mydb
+
+            Prompts then removes the databases containeddb and mydb on SQL Server sql2016
+
+        .EXAMPLE
+            PS C:\> Remove-DbaDatabase -SqlInstance sql2016 -Database containeddb -Confirm:$false
+
+            Does not prompt and swiftly removes containeddb on SQL Server sql2016
+
+    #>
     [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Low')]
     param (
         [parameter(Mandatory = $true, ValueFromPipeline = $true)]
